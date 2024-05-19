1792c8b549b81ebf2300dbd1deb47418fdbb9b42
Süeda Nur Sarıcan <148755086+suedasarican@users.noreply.github.com>
2024-05-19 13:39:02 +0300

    Update EmailSender.java

--- JavaTeamProject/src/main/java/org/example/EmailSender.java
+++ JavaTeamProject/src/main/java/org/example/EmailSender.java
@@ -7,6 +7,37 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class EmailSender{
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Email adresiniz: ");
+    String emailInput = scanner.nextLine();
+
+    final String userName = "suedanursarican233@gmail.com"; // Gönderen e-posta adresi
+    final String password = "t q s f h q s d s x l w j c v q"; // Gönderen e-posta şifresi
+
+    Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+    Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(userName, password);
+        }
+    });
+
+        try {
+        MimeMessage message = new MimeMessage(session);
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailInput, true));
+        message.setSubject("Mail Gönderildi");
+        message.setText("Mail gönderildi.");
+        System.out.println("Mail Gönderiliyor...");
+        Transport.send(message);
+        System.out.println("Mail Başarıyla Gönderildi....");
+    } catch (MessagingException me) {
+        System.out.println("Exception: " + me);
+    }
+
     
 }
 
