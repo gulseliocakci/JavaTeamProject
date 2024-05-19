61b1d7d0063b8d806b2a5a3ff3dfd8981ac85874
sevdegulsahin <sevdegulsahin25@gmail.com>
2024-05-19 11:59:47 +0300

    Update GUI.java

--- JavaTeamProject/src/main/java/org/example/GUI.java
+++ JavaTeamProject/src/main/java/org/example/GUI.java
@@ -3,11 +3,12 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class GUI {
+public class GUI  {
     public static void main(String[] args) {
 
     JFrame f= new JFrame("paralel dosya");
