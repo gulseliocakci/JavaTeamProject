3a62775c4fc8b8bcbaa487374088294d32674c6c
Süeda Nur Sarıcan <148755086+suedasarican@users.noreply.github.com>
2024-05-19 14:18:00 +0300

    Create module-info.java

---
+++ JavaTeamProject/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires javax.mail;
+
+    opens org.example to javafx.fxml;
+    exports org.example;
+}
