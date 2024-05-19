ae5ef7841de1a6b23587435e58c1619e9ae35f3e
Gülseli Ocakcı <154464410+gulseliocakci@users.noreply.github.com>
2024-05-19 14:44:48 +0300

    Update GUI.java

--- JavaTeamProject/src/main/java/org/example/GUI.java
+++ JavaTeamProject/src/main/java/org/example/GUI.java
@@ -15,60 +15,60 @@ import java.util.Properties;
 public class GUI {
     public static void GUIPlay(){
         // Swing GUI
-        JFrame f = new JFrame("paralel dosya");
-        f.setSize(750, 750);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame1 = new JFrame("paralel dosya");
+        frame1.setSize(750, 750);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JFrame f2 = new JFrame("mail");
-        f2.setSize(400, 200);
-        f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame2 = new JFrame("mail");
+        frame2.setSize(400, 200);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel l3 = new JLabel("Email adresinizi giriniz:");
-        l3.setBounds(20, 0, 200, 100);
+        JLabel lblEmail = new JLabel("Email adresinizi giriniz:");
+        lblEmail.setBounds(20, 0, 200, 100);
 
-        JTextArea s2 = new JTextArea();
-        s2.setBounds(200, 39, 200, 18);
+        JTextArea txtEmail = new JTextArea();
+        txtEmail.setBounds(200, 39, 200, 18);
 
-        JButton btn3 = new JButton("Giriş");
-        btn3.setBounds(200, 100, 100, 25);
+        JButton btnGiris = new JButton("Giriş");
+        btnGiris.setBounds(200, 100, 100, 25);
 
-        JTextArea s = new JTextArea();
-        s.setBounds(230, 25, 250, 20);
+        JTextArea txtKelime = new JTextArea();
+        txtKelime.setBounds(230, 25, 250, 20);
 
-        JLabel l1 = new JLabel("kelime sayısı:");
-        l1.setBounds(140, 28, 200, 100);
+        JLabel lblKelime = new JLabel("kelime sayısı:");
+        lblKelime.setBounds(140, 28, 200, 100);
 
-        JLabel l2 = new JLabel("karakter sayısı:");
-        l2.setBounds(400, 28, 200, 100);
+        JLabel lblKarakter = new JLabel("karakter sayısı:");
+        lblKarakter.setBounds(400, 28, 200, 100);
 
-        JTextArea textarea = new JTextArea();
-        textarea.setBounds(0, 100, 750, 680);
+        JTextArea textArea = new JTextArea();
+        textArea.setBounds(0, 100, 750, 680);
 
-        JButton btn2 = new JButton();
-        btn2.setBackground(Color.white);
-        btn2.setForeground(Color.PINK);
-        btn2.setBounds(535, 10, 50, 60); // Set button size to 100x50
+        JButton btnSearch = new JButton();
+        btnSearch.setBackground(Color.white);
+        btnSearch.setForeground(Color.PINK);
+        btnSearch.setBounds(535, 10, 50, 60); // Set button size to 100x50
 
         // Load the image for the "search" button
         String searchButtonImagePath = "C:\\Users\\sueda\\OneDrive\\Masaüstü\\grisearch.png";
         ImageIcon searchButtonIcon = new ImageIcon(searchButtonImagePath);
         Image searchButtonImage = searchButtonIcon.getImage();
         Image scaledSearchButtonImage = searchButtonImage.getScaledInstance(50, 60, Image.SCALE_SMOOTH);
-        btn2.setIcon(new ImageIcon(scaledSearchButtonImage));
+        btnSearch.setIcon(new ImageIcon(scaledSearchButtonImage));
 
-        btn2.addActionListener(new ActionListener() {
+        btnSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String text = textarea.getText();
+                String text = textArea.getText();
                 String[] words = text.split("\\s+");
-                l1.setText("kelime sayısı:" + (words.length));
-                l2.setText("karakter sayısı:" + (text.length() - 1));
+                lblKelime.setText("kelime sayısı:" + (words.length));
+                lblKarakter.setText("karakter sayısı:" + (text.length() - 1));
 
-                String emailInput = s2.getText(); // Kullanıcının girdiği e-posta adresi
+                String emailInput = txtEmail.getText(); // Kullanıcının girdiği e-posta adresi
 
                 // Mail gönderme işlemi
                 final String userName = "suedanursarican233@gmail.com"; // Gönderen e-posta adresi
-                final String password = "t q s f h q s d s x l w j c v q"; // Gönderen e-posta şifresi
+                final String password = "t q txtKelime frame1 h q txtKelime d txtKelime x l w fileChooser c v q"; // Gönderen e-posta şifresi
 
                 Properties props = new Properties();
                 props.put("mail.smtp.host", "smtp.gmail.com");
@@ -96,23 +96,23 @@ public class GUI {
             }
         });
 
-        JLabel lb = new JLabel("search word");
-        lb.setBounds(140, 0, 100, 70);
+        JLabel lblSearch = new JLabel("search word");
+        lblSearch.setBounds(140, 0, 100, 70);
 
-        JFileChooser j = new JFileChooser();
+        JFileChooser fileChooser = new JFileChooser();
 
         // Load the image for the "seç" button
         String selectButtonImagePath = "C:\\Users\\sueda\\OneDrive\\Masaüstü\\pembedosya.png";
-        JButton btn = new JButton();
-        btn.setBounds(20, 15, 70, 60); // Set button size to 100x100
-        btn.addActionListener(new ActionListener() {
+        JButton btnChooseFile = new JButton();
+        btnChooseFile.setBounds(20, 15, 70, 60); // Set button size to 100x100
+        btnChooseFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int returnVal = j.showOpenDialog(null);
+                int returnVal = fileChooser.showOpenDialog(null);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    File file = j.getSelectedFile();
+                    File file = fileChooser.getSelectedFile();
                     try {
-                        textarea.read(new FileReader(file.getAbsolutePath()), null);
+                        textArea.read(new FileReader(file.getAbsolutePath()), null);
                     } catch (IOException ex) {
                         System.out.println("Dosyaya ulaşırken bir hata oluştu: " + file.getAbsolutePath());
                     }
@@ -126,29 +126,29 @@ public class GUI {
         ImageIcon selectButtonIcon = new ImageIcon(selectButtonImagePath);
         Image selectButtonImage = selectButtonIcon.getImage();
         Image scaledSelectButtonImage = selectButtonImage.getScaledInstance(70, 60, Image.SCALE_SMOOTH);
-        btn.setIcon(new ImageIcon(scaledSelectButtonImage));
+        btnChooseFile.setIcon(new ImageIcon(scaledSelectButtonImage));
 
-        btn3.addActionListener(new ActionListener() {
+        btnGiris.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                f2.setVisible(false);
-                f.add(btn);
-                f.add(lb);
-                f.add(s);
-                f.add(btn2);
-                f.add(l1);
-                f.add(l2);
-                f.add(textarea);
-                f.setLayout(null);
-                f.setVisible(true);
+                frame2.setVisible(false);
+                frame1.add(btnChooseFile);
+                frame1.add(lblSearch);
+                frame1.add(txtKelime);
+                frame1.add(btnSearch);
+                frame1.add(lblKelime);
+                frame1.add(lblKarakter);
+                frame1.add(textArea);
+                frame1.setLayout(null);
+                frame1.setVisible(true);
             }
         });
 
-        f2.add(l3);
-        f2.add(s2);
-        f2.add(btn3);
-        f2.setLayout(null);
-        f2.setVisible(true);
+        frame2.add(lblEmail);
+        frame2.add(txtEmail);
+        frame2.add(btnGiris);
+        frame2.setLayout(null);
+        frame2.setVisible(true);
     }
 }
 
