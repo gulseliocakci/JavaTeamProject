cdbe90cd43d29eb415c6f5003542d09d4dbd37ec
sevdegulsahin <sevdegulsahin25@gmail.com>
2024-05-19 12:07:19 +0300

    update

--- JavaTeamProject/src/main/java/org/example/GUI.java
+++ JavaTeamProject/src/main/java/org/example/GUI.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class GUI  {
     public static void main(String[] args) {
+        public void GUI(){
 
     JFrame f= new JFrame("paralel dosya");
         f.setSize(750,750);
@@ -19,8 +20,8 @@ public class GUI  {
         l3.setBounds(20,0,150,100);
     JTextArea s2=new JTextArea();
         s2.setBounds(150,39,200,18);
-    JButton btn3=new JButton("giris");
-        btn3.setBounds(150,100,100,25);
+    JButton btnGiris=new JButton("giris");
+        btnGiris.setBounds(150,100,100,25);
 
     JTextArea s=new JTextArea();
         s.setBounds(230,25,250,20);
@@ -31,11 +32,11 @@ public class GUI  {
 
     JTextArea textarea=new JTextArea();
         textarea.setBounds(0,100,750,680);
-    JButton btn2=new JButton("search");
-        btn2.setBackground(Color.PINK);
-        btn2.setForeground(Color.PINK);
-        btn2.setBounds(505,10,100,50);
-        btn2.addActionListener(new ActionListener() {
+    JButton btnSearch=new JButton("search");
+        btnSearch.setBackground(Color.PINK);
+        btnSearch.setForeground(Color.PINK);
+        btnSearch.setBounds(505,10,100,50);
+        btnSearch.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             String text =textarea.getText();
@@ -49,11 +50,11 @@ public class GUI  {
     JLabel lb=new JLabel("search word");
         lb.setBounds(140,0,100,70);
     JFileChooser j=new JFileChooser();
-    JButton btn=new JButton("seç");
-        btn.setBackground(Color.MAGENTA);
-        btn.setForeground(Color.MAGENTA);
-        btn.setBounds(0,10,100,50);
-        btn.addActionListener(new ActionListener() {
+    JButton btnSec=new JButton("seç");
+        btnSec.setBackground(Color.MAGENTA);
+        btnSec.setForeground(Color.MAGENTA);
+        btnSec.setBounds(0,10,100,50);
+        btnSec.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             int returnVal = j.showOpenDialog(null);
@@ -73,14 +74,14 @@ public class GUI  {
     }
         );
 
-        btn3.addActionListener(new ActionListener() {
+        btnGiris.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
             f2.setVisible(false);
-            f.add(btn);
+            f.add(btnSec);
             f.add(lb);
             f.add(s);
-            f.add(btn2);
+            f.add(btnSearch);
             f.add(l1);
             f.add(l2);
             f.add(textarea);
@@ -92,9 +93,10 @@ public class GUI  {
 
         f2.add(l3);
         f2.add(s2);
-        f2.add(btn3);
+        f2.add(btnGiris);
         f2.setLayout(null);
         f2.setVisible(true);
+        }
 
 }
 }
