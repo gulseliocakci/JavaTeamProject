70056bbea85444bd5588c2107849e7d83fac90dc
sevdegulsahin <sevdegulsahin25@gmail.com>
2024-05-29 18:04:42 +0300

    Update Server.java

--- JavaTeamProject/src/main/java/org/example/Server.java
+++ JavaTeamProject/src/main/java/org/example/Server.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.io.*;
 
 public class Server extends dosyaGonder {
-    private static String[] serverIPs = {"192.168.43.237", "192.168.43.175", "192.168.43.121"}; // Sunucu IP adresleri
+    private static String[] serverIPs = {"192.168.43.237", "192.168.43.175", "192.168.43.77"}; // Sunucu IP adresleri
     private static int[] ports = {7755, 7755, 7756}; // Sunucu port numaraları
 
     public Server(File dosyaYolu, String txtKelime) {
