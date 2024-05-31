cb02b38e6fcb55d2f9a0acf1bf6b07a6e2dd3c65
sevdegulsahin <sevdegulsahin25@gmail.com>
2024-05-31 08:53:11 +0300

    up

--- JavaTeamProject/src/main/java/org/example/Server.java
+++ JavaTeamProject/src/main/java/org/example/Server.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.io.*;
 
 public class Server extends dosyaGonder {
-    private static String[] serverIPs = {"192.168.43.237", "192.168.43.175", "192.168.43.77"}; // Sunucu IP adresleri
+    private static String[] ClientIPs = {"192.168.43.237", "192.168.43.175", "192.168.43.77"}; // Sunucu IP adresleri
     private static int[] ports = {7756, 7755, 7756}; // Sunucu port numaraları
 
     public Server(File dosyaYolu, String arananKelime) {
@@ -39,8 +39,8 @@ public class Server extends dosyaGonder {
                 int start = (i == 0) ? 0 : splitIndices[i - 1];  //Ternary Operator
                 int end = (i == ClientSayisi - 1) ? metinStr.length() : splitIndices[i];
                 String part = metinStr.substring(start, end);
-                System.out.println("Sunucuya gönderiliyor: IP = " + serverIPs[i] + ", Port = " + ports[i]);
-                kelimeninGecmeSayisi += dosyaParcasiniGonder(part, arananKelime, serverIPs[i], ports[i], secilenDosya.getName());
+                System.out.println("Sunucuya gönderiliyor: IP = " + ClientIPs[i] + ", Port = " + ports[i]);
+                kelimeninGecmeSayisi += dosyaParcasiniGonder(part, arananKelime, ClientIPs[i], ports[i], secilenDosya.getName());
                 toplamKelimeSayisi = kelimeninGecmeSayisi;
             }
             System.out.println("Toplam kelime sayısı: " + toplamKelimeSayisi);
